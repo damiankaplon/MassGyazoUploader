fabbe2f48605b9fb50d722bbf2acf2d5ed7387dd
Damian Kaplon <kaplon.damian99@gmail.com>
2021-05-23 22:55:56 +0200

    Extend validation
    Extend validation so acces tokens are written to .gyazo file. IF user
    logs in for 2nd time he doesnt have to type in his acces token again

--- gyazo_uploader.py
+++ gyazo_uploader.py
@@ -1,14 +1,17 @@
 #!usr/bin/env/python3.9
+import sys
+
 from configed_parser import ConfigedParser
 import constant
 import requests
 import os
+import json
 
 
 class GyazoUploader:
     def __init__(self, parser: ConfigedParser):
         self.parser: ConfigedParser = parser
-        self.__access_token: str = self.parser.get_args().get('p')
+        self.__access_token: str = ""
         self.__files_to_upload: list = []
         self.__save_credentials()
         self.__upload()
@@ -17,22 +20,33 @@ class GyazoUploader:
         username: str = self.parser.get_args().get('u')
         try:
             with open(constant.CONFIG_PATH, 'r+') as config_file:
-                for line in config_file:
-                    if username in line:
-                        return None
-                config_file.write("Login: {} \n".format(username))
-                print("You login has been appended to config file")
+                data: dict = json.load(config_file)
+                for key, value in data.items():
+                    if key == username:
+                        for nd_key, nd_value in value.items():
+                            if nd_key == "access_token":
+                                self.__access_token = nd_value
+                                return
+
+                access_token: str = input("It is your first time here. Type in your access token: ")
+                json_to_append: dict = {username: {"access_token": access_token}}
+                data.update(json_to_append)
+                config_file.seek(0)
+                json.dump(data, config_file)
         except FileNotFoundError:
             print("Couldn't localize '.gyazo' file in home catalog")
 
     def __upload(self) -> None:
         directory: str = self.parser.get_args().get('dir')
-        dir_content = os.listdir(directory)
-        for element in dir_content:
-            if element.endswith(".jpg") or element.endswith(".png"):
-                self.__files_to_upload.append(element)
-        print(self.__files_to_upload)
-
+        try:
+            dir_content = os.listdir(directory)
+            for element in dir_content:
+                if element.endswith(".jpg") or element.endswith(".png"):
+                    self.__files_to_upload.append(element)
+            print(self.__files_to_upload)
+        except FileNotFoundError:
+            print("System can't find this directory!")
+            sys.exit(0)
         for file_path in self.__files_to_upload:
             self.__upload_image(file_path)
 
